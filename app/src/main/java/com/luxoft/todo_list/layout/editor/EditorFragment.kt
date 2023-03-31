20a0199693326e8e3e7673660e0abfa0bd631182
roblescristian4722 <roblescristian4722@gmail.com>
2023-03-31 02:25:24 -0600

    write/read from file

--- app/src/main/java/com/luxoft/todo_list/layout/editor/EditorFragment.kt
+++ app/src/main/java/com/luxoft/todo_list/layout/editor/EditorFragment.kt
@@ -1,5 +1,6 @@
 package com.luxoft.todo_list.layout.editor
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,8 @@ import com.luxoft.todo_list.databinding.FragmentEditorBinding
 import com.luxoft.todo_list.layout.common.Event
 import com.luxoft.todo_list.utils.Alert
 import javax.inject.Inject
+
+const val FILE_NAME = "output.csv"
 
 class EditorFragment : Fragment() {
     @Inject
@@ -30,6 +33,11 @@ class EditorFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.writeToCSV(FILE_NAME, requireContext(), "ejemplo", "desc")
+        val res = viewModel.readCSV(FILE_NAME, requireContext())
+        Alert("res: $res")
+
+
         // Listeners
         binding.fabCancel.setOnClickListener { findNavController().popBackStack() }
         binding.fabDone.setOnClickListener {
---
+++ app/src/main/java/com/luxoft/todo_list/models/Task.kt
@@ -0,0 +1,6 @@
+package com.luxoft.todo_list.models
+
+data class Task(
+    private val title: String = "",
+    private val description: String = ""
+)
